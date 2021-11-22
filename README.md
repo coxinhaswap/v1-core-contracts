a105a53049d5e009025a994899aa2c0ea235ac55
Gabriel <gabrielnabas@id.uff.br>
2021-11-22 16:48:00 -0300

    v0.1.0

---
+++ README.md
@@ -0,0 +1,21 @@
+# Coxinha Factory
+
+[![Actions Status](https://github.com/coxinhaswap/v1-core-contracts/workflows/CI/badge.svg)](https://github.com/coxinhaswap/v1-core-contracts/actions)
+
+In-depth documentation on CoxinhaSwap is available at [docs.coxinhaswap.finance](https://docs.coxinhaswap.finance/).
+
+# Local Development
+
+The following assumes the use of `node@>=10`.
+
+## Install Dependencies
+
+`yarn`
+
+## Compile Contracts
+
+`yarn compile`
+
+## Run Tests
+
+`yarn test`
---
+++ contracts/CoxinhaFactory.sol
@@ -0,0 +1,51 @@
+pragma solidity =0.5.16;
+
+import './interfaces/ICoxinhaFactory.sol';
+import './CoxinhaPair.sol';
+
+contract CoxinhaFactory is ICoxinhaFactory {
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(CoxinhaPair).creationCode));
+
+    address public feeTo;
+    address public feeToSetter;
+
+    mapping(address => mapping(address => address)) public getPair;
+    address[] public allPairs;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor(address _feeToSetter) public {
+        feeToSetter = _feeToSetter;
+    }
+
+    function allPairsLength() external view returns (uint) {
+        return allPairs.length;
+    }
+
+    function createPair(address tokenA, address tokenB) external returns (address pair) {
+        require(tokenA != tokenB, 'Coxinha: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Coxinha: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Coxinha: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(CoxinhaPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        ICoxinhaPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairs.push(pair);
+        emit PairCreated(token0, token1, pair, allPairs.length);
+    }
+
+    function setFeeTo(address _feeTo) external {
+        require(msg.sender == feeToSetter, 'Coxinha: FORBIDDEN');
+        feeTo = _feeTo;
+    }
+
+    function setFeeToSetter(address _feeToSetter) external {
+        require(msg.sender == feeToSetter, 'Coxinha: FORBIDDEN');
+        feeToSetter = _feeToSetter;
+    }
+}
